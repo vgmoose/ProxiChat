752740df7c278153c03318fd8b027e97fe5cd67b
Ricky Ayoub <vgmoose@gmail.com>
2014-11-17 17:57:38 -0500

    per

---
+++ Peer.h
@@ -0,0 +1,13 @@
+//
+//  Peer.h
+//  Proxi
+//
+//  Created by Ricky Ayoub on 11/6/14.
+//  Copyright (c) 2014 beta_interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Peer : NSObject
+
+@end
